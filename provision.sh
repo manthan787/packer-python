3182382d56e87cc1f3d9a922e1848400cd8423a8
manthan787 <manthant15@gmail.com>
2018-07-05 17:57:20 -0400

    Install python dependencies while provisioning

--- provision.sh
+++ provision.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 sleep 30
 
-sudo apt-get update
-sudo apt-get install python-pip python-dev build-essential
+sudo apt-get update -y
+sudo apt-get install python-pip python-dev build-essential -y
+
+sudo mkdir -p /home/opt/todo
+sudo chown ubuntu /home/opt/todo
+git clone https://github.com/manthan787/packer-python /home/opt/todo
+pip install -r /home/opt/todo/requirements.txt
